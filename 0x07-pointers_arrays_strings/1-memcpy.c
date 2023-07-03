5a802ff614a742b21e849293dbce09a5c016266e
Areola Joshua <areolajoshua@gmail.com>
2023-07-03 16:23:22 +0100

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - a function that copies memory area
+ * Return: copied memory with n byted change
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = o;
+	int i = n;
+
+	for (; rr < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
